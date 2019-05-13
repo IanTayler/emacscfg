cff4bc485162b8854d91e40c5e51374e0374b100
Ian Tayler <iangtayler@gmail.com>
2019-05-12 21:01:30 -0300

    Add initial minimal configuration.

---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Packages
+(require 'evil)
+(evil-mode 1)
+
+;; Get a less shitty theme
+(load-theme 'deeper-blue t)
+
+;; Get rid of the toolbar
+(tool-bar-mode -1)
